5541a8240f2f444178c18daed510a601e1d805d0
LikeNo1Else <socratez.hf@gmail.com>
2025-07-09 23:55:35 +0000

    keys, s3 buckets, other

---
+++ Class2/main.tf
@@ -0,0 +1,16 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "kaizen-alexander-super-rare-name"
+  force_destroy = true
+}
+
+resource "aws_s3_object" "object" {
+  depends_on = [aws_s3_bucket.example]
+  bucket = "kaizen-alexander-super-rare-name"
+  key    = "main.tf"
+  source = "main.tf"
+}
