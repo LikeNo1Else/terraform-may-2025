9df88632cc94f2d69d16afb1e1881787471628f9
LikeNo1Else <socratez.hf@gmail.com>
2025-07-16 23:28:21 +0000

    Variables

---
+++ Class4-Part1/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+variable "region" {} 
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_key_pair" "deployer" {
+ key_name = var.key_name
+ public_key = file("~/.ssh/id_rsa.pub")
+}
+
+variable key_name{}
