7cc41a0cca322816ad4fbb083f7bf38e8aeb2c7f
LikeNo1Else <socratez.hf@gmail.com>
2025-07-22 01:51:12 +0000

    provisioner

--- Class5-Part2/main.tf
+++ Class5-Part2/main.tf
@@ -7,19 +7,54 @@ terraform {
   }
 }
  
-
 provider "aws" {
   region = "us-east-1"
 }
 
-
 resource "aws_key_pair" "deployer" {
- key_name = "class5-key"
+ key_name = "my-bastion-key"
  public_key = file("~/.ssh/id_rsa.pub")
+}
 
- provisioner local-exec {
-  command = "mkdir kaizen"
+resource "aws_instance" "web" {
+  ami           = "ami-0cbbe2c6a1bb2ad63"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id] 
    
- }
+   connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host     = aws_instance.web.public_ip
+  }
 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo systemctl start httpd",
+      "sudo systemctl enable httpd"
+    ]    
+  }
+}
+
+resource "null_resource" "hello" {
+    connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host     = aws_instance.web.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo systemctl start httpd",
+      "sudo systemctl enable httpd"
+    ]    
+  } 
+}
+
+output "kaizen" {
+  value = aws_instance.web.public_ip
 }
