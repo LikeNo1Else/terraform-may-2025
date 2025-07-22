df3ae9ea2b827d1cba7fc785e04946277b8f813f
LikeNo1Else <socratez.hf@gmail.com>
2025-07-22 00:18:21 +0000

    Make file

--- Class5-Part1/main.tf
+++ Class5-Part1/main.tf
@@ -7,13 +7,14 @@ terraform {
   }
 }
  
+
 provider "aws" {
   region = var.region
 }
 
+
 resource "aws_key_pair" "deployer" {
  key_name = var.key_name
  public_key = file("~/.ssh/id_rsa.pub")
-
 
 }
