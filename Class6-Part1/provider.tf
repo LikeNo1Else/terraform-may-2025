14e151185191e4973d9916822a0d99eff123607b
LikeNo1Else <socratez.hf@gmail.com>
2025-07-24 00:09:12 +0000

    Updated

--- Class6-Part1/provider.tf
+++ Class6-Part1/provider.tf
@@ -11,3 +11,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+
+module "may" {
+  source  = "LikeNo1Else/may/vpc"
+  version = "1.0.0"
+
+
+  vpc_cidr =  ["10.0.0.0/16"]
+  subnet1_cidr  = ["10.0.1.0/24"]
+  subnet2_cidr  = ["10.0.2.0/24"]
+  subnet3_cidr  = ["10.0.3.0/24"]
+
+
+}
