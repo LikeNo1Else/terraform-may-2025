362b04d16b247121c7ee1aec6371bf2a4ba7a4da
LikeNo1Else <socratez.hf@gmail.com>
2025-08-01 02:29:31 +0000

    Modules

--- Modules/github/main.tf
+++ Modules/github/main.tf
@@ -13,11 +13,12 @@ provider "aws" {
 
 
 module "may" {
-  source  = "git@github.com:LikeNo1Else/terraform-vpc-may.git?ref=v1.0.0"
-
-  vpc_cidr =  "172.31.0.0/16"
-  subnet1_cidr  = "172.31.1.0/24"
-  subnet2_cidr  = "172.31.2.0/24"
-  subnet3_cidr  = "172.31.3.0/24"
-  # vpc_name = "kaizen"
+  source  = "LikeNo1Else/may/vpc"
+  version = "2.0.0"
+  vpc_cidr = "172.31.0.0/16"
+  subnet1_cidr = "172.31.1.0/24"
+  subnet2_cidr = "172.31.2.0/24" 
+  subnet3_cidr = "172.31.3.0/24"
+  vpc_name = "kaizen"
 }
+
