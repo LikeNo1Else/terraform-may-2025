db74d249ddb562fe3467e5b68b034483312a4158
LikeNo1Else <socratez.hf@gmail.com>
2025-08-01 06:20:17 +0000

    Modules foldres

--- Modules/my-module/ec2/main.tf
+++ Modules/my-module/ec2/main.tf
@@ -1,9 +1,27 @@
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id = aws_subnet.main1.id
+  subnet_id = var.subnet_id
 }
 
-variable ami_id { default = "ami-0cbbe2c6a1bb2ad63" }
-variable instance_type { default = "t2.micro" }
-# variable subnet_id { default = }
+variable ami_id {}
+variable instance_type {}
+variable subnet_id {}
+
+provider aws {
+  region = var.region
+}
+
+variable region {}
+
+
+# data terraform_remote_state vpc {
+#   backend = "local"
+#   config = { 
+#     path = "../vpc/terraform.tfstate"
+#     }
+# }
+
+# output hello {
+#   value = data.terraform_remote_state.vpc
+# }
--- Modules/my-module/vpc/main.tf
+++ Modules/my-module/vpc/main.tf
@@ -1,8 +1,3 @@
-provider aws {
-region = "us-east-1"
-}
-
-
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 
@@ -10,7 +5,6 @@ resource "aws_vpc" "main" {
     Name = "${var.environment}-vpc"
   }
 }
-
 resource "aws_subnet" "main1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.subnet1_cidr
@@ -20,10 +14,18 @@ resource "aws_subnet" "main1" {
   }
 }
 
-variable vpc_cidr { default = "10.0.0.0/16"}
-variable subnet1_cidr { default = "10.0.1.0/24"}
-variable environment { default = "dev" }
 
-# output subnet {
-#   value = aws_subnet.main1.id
-# }
+variable vpc_cidr {}
+variable subnet1_cidr {}
+variable environment {}
+
+
+output subnet {
+  value = aws_subnet.main1.id
+}
+
+provider aws {
+  region = var.region
+}
+
+variable region {}
